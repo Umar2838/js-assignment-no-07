cdd3abefd74b41186091a09b1115f1494a6f0cec
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-03-20 23:59:18 +0500

    added

---
+++ app.js
@@ -0,0 +1,71 @@
+// <*****************************************Chapter 26 To 30********************************************************>
+
+//Question No 01
+
+// var positiveInteger = prompt("Enter Positive Integer")
+
+// var number = positiveInteger
+// var round = Math.round(positiveInteger)
+// var floor = Math.floor(positiveInteger)
+// var ceil = Math.ceil(positiveInteger)
+// console.log("Number: "+number+"\n"+"Round Off value: "+round+"\n"+"Floor Value"+floor+"\n"+"Ceil Value"+ceil)
+
+// Question no 02
+
+// var negativeInteger = prompt("Enter Negative Integer")
+// var number = negativeInteger
+// var round = Math.round(negativeInteger)
+// var floor = Math.floor(negativeInteger)
+// var ceil = Math.ceil(negativeInteger)
+// console.log("Number: "+number+"\n"+"Round Off value: "+round+"\n"+"Floor Value"+floor+"\n"+"Ceil Value"+ceil)
+
+// Question no 03
+
+// var value = prompt("Enter value for Absolute Value")
+// var abs = Math.abs(value)
+// console.log("The absolute value of "+value+" is "+abs) 
+
+// Question no 04
+
+// var dice = Math.random()*6
+// console.log("Random Dice Value is : " + Math.round(dice))
+
+// Question no 05
+
+// var game = Math.random()*2
+// var result = Math.ceil(game)
+
+// if(result==1){
+
+//     console.log("Random Coin Value is : Head" )
+
+// }
+// else{
+//     console.log("Random Coin Value is : Tails")
+// }
+
+// Question no 06
+
+// var number = Math.random()*100
+// console.log("Random number Between 1 and 100: "+Math.round(number))
+
+// Question no 07
+
+// var weight = prompt("Enter your weight")
+
+// console.log("The weight of user is: "+ weight.toString())
+
+// Question no 08
+
+// var randomSecret = Math.round(Math.random()*10)
+
+// var userInput = prompt("Enter Secret Number")
+
+// if(randomSecret==userInput){
+//     console.log("Conragulation You found that Number")
+// }
+// else{
+//     console.log("Sorry,Try Again")
+// }
+ 
+ 
